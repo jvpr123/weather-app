5be7fafa3306fc2d9ac53ed55d9795499389807f
João Victor P. Rodrigues <jvpr123@outlook.com>
2026-09-02 11:44:06 -0300

    chore(ci): add project quality scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,27 @@
 import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import pluginVue from 'eslint-plugin-vue';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    {
-        ignores: ['node_modules', 'public/build', 'vendor'],
+  {
+    ignores: ['node_modules', 'public/build', 'vendor'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['resources/js/**/*.{ts,vue}'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
     },
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...pluginVue.configs['flat/recommended'],
-    {
-        files: ['resources/js/**/*.{ts,vue}'],
-        languageOptions: {
-            globals: globals.browser,
-            parserOptions: {
-                parser: tseslint.parser,
-            },
-        },
-        rules: {
-            'vue/multi-word-component-names': 'off',
-        },
+    rules: {
+      'vue/multi-word-component-names': 'off',
     },
+  },
+  eslintConfigPrettier,
 );
